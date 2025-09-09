448ae72c68994104573e97d6758330212c897e0f
raihan1846 <raihan.bd2022@gmail.com>
2025-09-09 22:19:25 +0600

    spinner add

--- script.js
+++ script.js
@@ -25,23 +25,46 @@ fetch(url)
                 btn.classList.remove("bg-[#16a34a]", "text-white")
              })
             }
-            btn.classList.add("bg-[#16a34a]", "text-white")
-
+            btn.classList.add("bg-[#16a34a]", "text-white");
+            manageSpinner(true);
+            setTimeout(() => {
             if (dataCategory === "All") {
                 displayPlants(allPlants);
             }else{
                 const filterPlant = allPlants.filter(data => data.category == dataCategory)
                 displayPlants(filterPlant)
             }
+        }, 100);
            })
          })
     })
 
  
+    // const manageSpinner = (status)=>{
+    //     if(status == true){
+    //         document.getElementById("spinner").classList.remove("hidden");
+    //         document.getElementById("plantList").classList.add("hidden");
+    //     }else{
+    //         document.getElementById("plantList").classList.remove("hidden");
+    //         document.getElementById("spinner").classList.add("hidden");
+    //     }
+    // }
+    const manageSpinner = (status) => {
+        const spinner = document.getElementById("spinner");
+        const plantList = document.getElementById("plantList");
+        if (status) {
+            spinner.classList.remove("hidden");
+            plantList.classList.add("hidden");
+        } else {
+            spinner.classList.add("hidden");
+            plantList.classList.remove("hidden");
+        }
+    }
 // plants part 
-
+let total = 0;
 const plantUrl = `https://openapi.programming-hero.com/api/plants`;
 const plantList = document.getElementById('plantList');
+manageSpinner(true);
 fetch(plantUrl)
     .then(res => res.json())
     .then(data => {
@@ -59,20 +82,91 @@ fetch(plantUrl)
                   class="rounded-xl w-full h-48 md:h-56 lg:h-64 object-cover" />
               </figure>
               <div class="card-body">
-                <h2 class="card-title">${plant.name}</h2>
+                <h2 class="card-title plantName" onclick="loadWordDetail(${plant.id})">${plant.name}</h2>
                 <p>${plant.description}</p>
                 <div class="flex justify-between w-full">
                   <button class="bg-[#DCFCE7] px-2 py-1 rounded-full">${plant.category}</button>
-                  <p class="mr-3 text-right">$ ${plant.price}</p>
+                  <p class="mr-3 text-right">$ <span class="plantPrice">${plant.price}</span></p>
                 </div>
               
                 <div class="card-actions">
-                  <button class="btn w-full rounded-full bg-[#15803d]">Add to Cart</button>
+                  <button class="btn w-full rounded-full bg-[#15803d] add-to-card">Add to Cart</button>
                 </div>
               </div>
 
         `;
             plantList.appendChild(div);
 
-        })
+        });
+       manageSpinner(false);
+
+
+        const addToCard = document.querySelectorAll('.add-to-card');
+        addToCard.forEach(btn =>{
+        btn.addEventListener('click',() => {
+            const card = btn.closest('.card'); 
+            const plantName = card.querySelector('.plantName').textContent;
+            const plantPrice = parseFloat(card.querySelector('.plantPrice').textContent);
+            total+=plantPrice;
+            alert(`Product Name : ${plantName} and Price : ${plantPrice}`);
+            document.getElementById('totalPrice').textContent = total;
+            const addCard = document.getElementById('addCard');
+            const div = document.createElement('div');
+            div.innerHTML = `
+            <div class="flex justify-between items-center mx-10 bg-[#F0FDF4] p-2 rounded-md space-y-2 mb-4">
+          <div class="">
+            <h2>${plantName}</h2>
+            <p><span class="currency">$</span> <span class="pre">${plantPrice}</span></p>
+          </div>
+          <div>
+            <h2 class="remove-item">X</h2>
+          </div>
+        </div>
+            `;
+            addCard.appendChild(div);
+
+            div.querySelector('.remove-item').addEventListener('click', ()=>{
+             div.remove();
+             total-=plantPrice;
+            document.getElementById('totalPrice').textContent = total;
+            })
+       })
+    })
+
+       
+    
     }
+
+    const loadWordDetail = async (id)=>{
+        const url = `https://openapi.programming-hero.com/api/plant/${id}`;
+        const res = await fetch(url);
+        const details = await res.json();
+        displayWordDetails(details.plants)
+    }
+
+    const displayWordDetails = (words) =>{
+        const details = document.getElementById('detailContainer');
+        details.innerHTML = `
+        <figure class="px-6 pt-6">
+        <img src="${words.image}" alt="Tree"
+          class="rounded-xl w-full h-48 md:h-56 lg:h-64 object-cover" />
+      </figure>
+      <div class="card-body">
+        <h2 class="card-title plantNames">${words.name}</h2>
+        <p>${words.description}</p>
+        <div class="flex justify-between w-full">
+          <button class="bg-[#DCFCE7] px-2 py-1 rounded-full">${words.category}</button>
+          <p class="mr-3 text-right plantPrices">$ ${words.price}</p>
+        </div>
+        <div class="modal-action">
+      <form method="dialog">
+        <button class="btn">Close</button>
+      </form>
+    </div>
+        `;
+        document.getElementById('word_modal').showModal();
+    }
+
+    
+   
+    
